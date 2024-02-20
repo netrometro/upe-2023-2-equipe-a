6b7a64a7fecc47aa208e27681ba1b1215f7f8b2c
Antonio Sereno <antoniojsereno@gmail.com>
2024-02-20 08:37:38 -0300

    task 1
    - fazer uma pagina frontend de teste

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+//import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+      <h1>Projeto teste Gerencia de Configuraçao</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
