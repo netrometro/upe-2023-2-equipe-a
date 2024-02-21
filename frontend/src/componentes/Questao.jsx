645d5e24f6201ce7bec18d11fc6af4b094f1ccea
Antonio Sereno <antoniojsereno@gmail.com>
2024-02-21 12:11:47 -0300

    Criaçao do componente Questao
    Componente criado para represntar uma questao nesse sistema de banco de questoes. O componente ainda nao consegue puxar informaçoes do back por falta do mesmo, e por esse motivo foi criada uma constante para testar o componente no ambito do frontend. Agora irei tentar criar as rotas do back, vamos ver oq acontece.

---
+++ frontend/src/componentes/Questao.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Questao = ({ id, titulo, alternativas, resposta }) => {
+  return (
+    <div className="questao-prova">
+      <h3>{titulo}</h3>
+      <ul>
+        {alternativas.map((alternativa, index) => (
+          <li key={index}>{alternativa}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Questao;
