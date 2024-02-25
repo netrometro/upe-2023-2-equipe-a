f34b189339445f1198f79fcdc782f9ce6ecd2642
Antonio Sereno <antoniojsereno@gmail.com>
2024-02-24 22:33:24 -0300

    Rota Questoes com problema. resolver amanha

---
+++ backend/src/routes.js
@@ -0,0 +1,25 @@
+//Arquivo para rotas 
+const express = require("express");
+
+const Questoes = [{ name: 'Brasil'}];
+const Routes = express.Router(); //variavel para chamada do Router
+
+//CRUD -> Rotas do CRUD
+//C
+Routes.post("/Questoes", (res, req) => {
+  const { name } = req.body?.titulo || {};
+
+  Questoes.push({ name });
+  return res.status(201).json(Questoes);
+});
+
+//R
+
+//U
+
+//D
+
+
+
+//exportando as rotas
+module.exports = Routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,21 @@
-//Antonio Sereno 
 
 //express
 const express = require('express')
-
 const app = express()
-
 app.use(express.json())
 
-
 //Rotas
+const Routes = require('./routes')
+app.use(Routes)
+
+
+
 app.listen(3333, () => console.log("Server runnig in 3333"))
+
+app.get('/', (req, res) => {
+  // 'res' is a valid response object here
+  res.status(200).send('Hello, World!');
+});
 
 app.get("/saude", (req, res) => {
   return res.json("Up");
