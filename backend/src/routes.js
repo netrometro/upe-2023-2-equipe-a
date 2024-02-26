f9facbea596177d90f202792c658ef09e7e585b2
Antonio Sereno <antoniojsereno@gmail.com>
2024-02-26 18:20:36 -0300

    Questao Delete Funcional

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -27,19 +27,19 @@ Routes.get("/listarTodasQuestoes" , async (request, response) => {
   return response.status(200).json(listaTodasQestoes);
 })
 //U
-Routes.put("/UpdateQuestoes", async (request, response) => {
-
+Routes.put("/UpdateQuestao", async (request, response) => {
+  //, titulo, Alternativas, resposta
   const {id, titulo, Alternativas, resposta} = request.body;
 
   //verifica se o id existe
   if (!id) {
-    return response.status(400).json("Id is mandatory");
+    return response.status(400).json("Id é obrigatorio");
   }
 
   //verifica se a questao existe
-  const QuestaoExiste = await prisma.todo.findUnique({ where: { id } });
-  if (!todoAlreadyExist) {
-    return response.status(404).json("Todo not exist");
+  const QuestaoExiste = await prisma.questao.findUnique({ where: { id } });
+  if (!QuestaoExiste) {
+    return response.status(404).json("Questao nao exite");
   }
 
   //Faz a substituiçao dos valores
@@ -65,19 +65,19 @@ Routes.delete("/DeletarQuestao/:id", async (request, response) => {
   //verifica de o ID veio na URL
   const intId = parseInt(id);
   if (!intId) {
-    return response.status(400).json("Id is mandatory");
+    return response.status(400).json("Id é obrigatorio");
   }
 
   //verifica se a questao realmente existe
-  const QuestaoAlreadyExist = await prisma.todo.findUnique({
+  const QuestaoExiste = await prisma.questao.findUnique({
     where: { id: intId },
   });
-  if (!todoAlreadyExist) {
-    return response.status(404).json("Todo not exist");
+  if (!QuestaoExiste) {
+    return response.status(404).json("Questao nao existe");
   }
 
   //questao existe, manda dleetar
-  await prisma.todo.delete({ where: { id: intId } });
+  await prisma.questao.delete({ where: { id: intId } });
 
   //retorna ok pra operaçao de deletar a questao pelo id
   return response.status(200).send();
