d234b4d85b22779c00b37045c9c7b64be2eea5e7
Antonio Sereno <antoniojsereno@gmail.com>
2024-02-25 21:55:55 -0300

    Rota Update Questoes Nao funcional

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,7 +4,8 @@ const Routes = express.Router(); //variavel para chamada do Router
 const {PrismaClient} = require("@prisma/client");
 const prisma = new PrismaClient();
 
-//CRUD -> Rotas do CRUD
+
+//CRUD Questoes -> Rotas do CRUD para Questoes
 //C
 Routes.post("/CriarQuestoes", async (request, response) => {
   const { titulo ,Alternativas,resposta  } = request.body;
@@ -15,22 +16,51 @@ Routes.post("/CriarQuestoes", async (request, response) => {
       Alternativas: Alternativas,
       resposta: resposta,
     }
-  })
+  });
   return response.status(201).json(novaQuestao);
 });
-
 //R
 Routes.get("/listarTodasQuestoes" , async (request, response) => {
 
   const listaTodasQestoes = await prisma.questao.findMany();
 
-  return response.status(200).json(listaTodasQestoes)
+  return response.status(200).json(listaTodasQestoes);
 })
-
 //U
+Routes.put("/UpdateQuestoes", async (request, response) => {
 
+  const {id, titulo, Alternativas, resposta} = request.body;
+
+  const update = await prisma.questao.put({
+    where: {
+      id,
+    },
+     data:{
+      titulo: titulo,
+      Alternativas: Alternativas,
+      resposta: resposta,
+     },
+  });
+
+  return response.status(200).json(update);
+
+})
 //D
 
+
+
+//CRUD Alternativas -> Rotas do CRUD para Alternativas
+//C
+//R
+//U
+//D
+
+
+//CRUD Usuario -> Rotas do CRUD para Usuario
+//C
+//R
+//U
+//D
 
 
 //exportando as rotas
