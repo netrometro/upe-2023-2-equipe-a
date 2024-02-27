26db633a4d8ff65d2f6b83f59a31f294f33601e3
Antonio Sereno <antoniojsereno@gmail.com>
2024-02-27 08:14:05 -0300

    Entrega 2 - formulario para criação de nova questao

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import QuestaoForm from './componentes/Questoes/QuestaoForm'
 import QuestaoList from './componentes/Questoes/QuestaoList'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <div>
       <h1>Banco de Questões e Formulador de Provas</h1>
       <br/>
+      <QuestaoForm />
       <br/>
       <QuestaoList />
       </div>
